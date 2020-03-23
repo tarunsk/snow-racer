d470777e708c1655eeacea3fbdf45059dfa43246
Tarun Khubchandani <tarun@kfit.com>
2020-03-23 17:51:25 -0400

    Refactor and prep for jumps

--- src/components/game.vue
+++ src/components/game.vue
@@ -6,49 +6,40 @@ export default {
   },
   methods: {
     handleKeyPress: function (e) {
-      const upArrow = 38;
-      const downArrow = 40;
       const leftArrow = 37;
       const rightArrow = 39;
       const keyCode = e.keyCode;
-      if (keyCode === upArrow) {
-        console.log("moving forward");
-        this.racerYpos += 10;
-      }
-      if (keyCode === downArrow) {
-        console.log("moving backward");
-        this.racerYpos -= 10;
-      }
       if (keyCode === rightArrow) {
         console.log("turning right");
-        this.racerXpos += 10;
+        this.racerXpos = (parseFloat(this.racerXpos) + 0.5) + '%';
       }
       if (keyCode === leftArrow) {
         console.log("turning left");
-        this.racerXpos -= 10;
+        this.racerXpos = (parseFloat(this.racerXpos) - 0.5) + '%';
       }
-    }
+    },
+
   },
   data: function() {
     return {
       graduation: require('@/assets/img/graduation.png'),
       racerXpos: null,
       racerYpos: null,
+      availableJumps: 0,
     }
   },
   mounted: function () {
     // add an event listener for keypress
-    console.log(this.$refs.gameBoard.clientWidth);
     window.addEventListener('keydown', this.handleKeyPress);
-    this.racerXpos = this.$refs.gameBoard.clientWidth/2;
-    this.racerYpos = "0%";
-  }
+    this.racerXpos = "40%";
+    this.racerYpos = "5%";
+  },
 }
 </script>
 
 <template>
   <div ref="gameBoard" class="container h-100 w-100 game-board">
     <img ref="graduation" class="graduation" v-bind:src="this.graduation" />
-    <img v-bind:src="icon" ref="avatar" class="avatar" v-bind:style="{ bottom: this.racerYpos, left: this.racerXpos + 'px' }" />
+    <img v-bind:src="icon" ref="avatar" class="avatar" v-bind:style="{ bottom: this.racerYpos, left: this.racerXpos }" />
   </div>
 </template>
