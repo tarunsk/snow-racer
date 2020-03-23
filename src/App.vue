211bdeeac6aa4bb91369b43ed7a7ad41eb0279ba
Tarun Khubchandani <tarun@kfit.com>
2020-03-22 20:39:06 -0400

    Having a touch time with this

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,8 @@ export default {
       renderGame: false,
       chosenTrack: null,
       chosenIcon: null,
+      racerXpos: 0,
+      racerYpos: 0,
     }
   },
   methods: {
@@ -27,15 +29,41 @@ export default {
     setChosenIcon(value) {
       this.chosenIcon = value;
       this.renderGame = true;
-      console.log(this.renderGame);
+    },
+    handleKeyPress: function (e) {
+      const upArrow = 38;
+      const downArrow = 40;
+      const leftArrow = 37;
+      const rightArrow = 39;
+      const keyCode = e.keyCode;
+      if (keyCode === upArrow) {
+        console.log("moving forward");
+        this.racerXpos += 10;
+      }
+      if (keyCode === downArrow) {
+        console.log("moving backward");
+        this.racerXpos -= 10;
+      }
+      if (keyCode === rightArrow) {
+        console.log("turning right");
+        this.racerYpos += 10;
+      }
+      if (keyCode === leftArrow) {
+        console.log("turning left");
+        this.racerYpos -= 10;
+      }
     }
   },
+  mounted: function () {
+    // add an event listener for keypress
+      window.addEventListener('keydown', this.handleKeyPress)
+  }
 }
 </script>
 
 <template>
-  <div id="app" class="h-100 bg">
-    <Landing v-if="!menuSelect" v-on:play-now="menuSelect=true"/>
+  <div ref="app" id="app" class="h-100 bg">
+    <Landing ref='landing' v-if="!menuSelect" v-on:play-now="menuSelect=true"/>
     <div class="row h-100 w-100 my-auto mx-auto track-select" v-if="menuSelect && (chosenTrack === null)">
       <Menu v-for="track in Tracks.options" v-bind:key="track.id" v-bind:option="track" v-on:chosen-option="setChosenTrack"/>
     </div>
@@ -43,8 +71,8 @@ export default {
       <Menu v-for="icon in Icons.options" v-bind:key="icon.id" v-bind:option="icon" v-on:chosen-option="setChosenIcon"/>
     </div>
     <div class="h-100" v-if="renderGame === true">
-      <div class="racetrack h-100 mx-auto my-auto" v-html="chosenTrack" />
-      <div class="avatar h-25" v-html="chosenIcon" />
+      <div class="racetrack h-100 w-100 mx-auto my-auto" v-html="chosenTrack " />
+      <div class="avatar" v-bind:style="{ top: this.racerYpos + 'px', left: this.racerXpos + 'px'}" v-html="chosenIcon" />
     </div>
   </div>
 </template>
