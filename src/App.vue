f648f2d0e552569f37c6ea73f3ae88ef3ce93f73
Danielle Newport <dnewpo@umich.edu>
2020-03-24 17:48:07 -0400

    added functions to use for car animation

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import Landing from './components/landing.vue'
 import Menu from './components/menu.vue'
 import Tracks from './assets/tracks.json'
 import Icons from './assets/icons.json'
+import TWEEN from '@tweenjs/tween.js';
 
 export default {
   name: 'App',
@@ -20,6 +21,17 @@ export default {
       chosenIcon: null,
       racerXpos: 0,
       racerYpos: 0,
+    }
+  },
+  computed:{
+    originX(){
+        return this.x - this.width/2
+    },
+    originY(){
+        return this.y - this.height/2
+    },
+    transform(){
+        return `rotate(${this.angle} ${this.x} ${this.y})`
     }
   },
   methods: {
@@ -52,7 +64,67 @@ export default {
         console.log("turning left");
         this.racerYpos -= 10;
       }
+    },
+    // Starts speeding tween
+    start(dir,type){
+        this[type].pressed[dir] = true
+        this[type].dir = dir
+        if (this[type].tween) this[type].tween.stop()
+        this[type].tween = new TWEEN.Tween(this[type])
+            .to({speed: dir},400)
+            .easing(TWEEN.Easing.Quadratic.In)
+            .start()
+    },
+    // Slow the speed down to 0
+    stop(dir,type){
+      this[type].pressed[dir] = false
+      // Only stop if dir is the current dir
+      if (this[type].dir == dir){
+          // Check if there is previous dir active
+          if (this[type].pressed[dir*-1]){
+              this.start(dir*-1,type)
+          }else{
+              this[type].tween.stop()
+              this[type].tween = new TWEEN.Tween(this[type])
+                  .to({speed: 0},200)
+                  .easing(TWEEN.Easing.Quadratic.Out)
+                  .start()
+          }
+      }
     }
+  },
+  created(){
+    let pressed = {}
+    window.addEventListener('keydown',e=>{
+        // Block successive events of the same key
+        if (pressed[e.keyCode]) return
+        else pressed[e.keyCode] = 1
+        if (e.keyCode == 37) this.start(-1,'turn')       // Left
+        else if (e.keyCode == 39) this.start(1,'turn')   // Right
+        else if (e.keyCode == 38) this.start(1,'move')   // Up
+        else if (e.keyCode == 40) this.start(-1,'move')  // Down
+    })
+    window.addEventListener('keyup',e=>{
+        delete pressed[e.keyCode]
+        if (e.keyCode == 37) this.stop(-1,'turn')
+        else if (e.keyCode == 39) this.stop(1,'turn')
+        else if (e.keyCode == 38) this.stop(1,'move')
+        else if (e.keyCode == 40) this.stop(-1,'move')
+    })
+    this.turn = {
+        speed: 0,
+        dir:0,
+        pressed:{'-1':false,'1':false},
+        tween: null
+    }
+    this.move = {
+        speed: 0,
+        dir:0,
+        pressed:{'-1':false,'1':false},
+        tween: null
+    }
+    this.lastFrame = Date.now()
+    this.render()
   },
   mounted: function () {
     // add an event listener for keypress
