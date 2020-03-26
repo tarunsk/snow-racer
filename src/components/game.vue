84aa7c7aa15be55579680341fbb39ee1de1676c3
Danielle Newport <dnewpo@umich.edu>
2020-03-26 14:57:32 -0400

    game win changes

--- src/components/game.vue
+++ src/components/game.vue
@@ -115,10 +115,16 @@ export default {
 
 <template>
   <div ref="gameBoard" class="container h-100 w-100 game-board">
-    <img ref="graduation" class="graduation" v-bind:src="this.graduation" />
-    <img v-bind:src="icon" ref="avatar" class="avatar" v-bind:style="{ bottom: this.racerYpos, left: this.racerXpos }" />
-    <div v-for="item in negItemList" v-bind:key="item.id" class="obstacle">
-      <img v-bind:src="item.src" v-bind:id="item.id" v-bind:ref="item.id" v-bind:style="{ top: item.ypos, left: item.xpos }"/>
+    <div ref="playGame" v-if="!gameWin">
+      <img ref="graduation" class="graduation" v-bind:src="this.graduation" />
+      <img v-bind:src="icon" ref="avatar" class="avatar" v-bind:style="{ bottom: this.racerYpos, left: this.racerXpos }" />
+      <div v-for="item in negItemList" v-bind:key="item.id" class="obstacle">
+        <img v-bind:src="item.src" v-bind:id="item.id" v-bind:ref="item.id" v-bind:style="{ top: item.ypos, left: item.xpos }"/>
+      </div>
+    </div>
+
+    <div ref="winScreen" v-if="gameWin">
+      <h1> Diploma pic will go here lol </h1>
     </div>
   </div>
 </template>
