d27278eb6a6cfa65b0004d1ab443d003e8bfd534
Kartik Pandit <kpandit@umich.edu>
2020-03-26 16:49:19 -0400

    Remove VueCollision dependency

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueCollision from 'vue-collision'
 
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
@@ -13,7 +12,6 @@ export const eventBus = new Vue();
 
 Vue.config.productionTip = false
 // collision
-Vue.use(VueCollision, { globalTriggers: ['resize', 'scroll'] })
 
 new Vue({
   render: h => h(App),
