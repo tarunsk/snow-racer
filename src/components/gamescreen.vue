56afd758db3d3912e0bbb67b129482c7ba78f6a0
Kartik Pandit <kpandit@umich.edu>
2020-03-29 14:05:52 -0400

    Send time to finish from Countdown to GameScreen

--- src/components/gamescreen.vue
+++ src/components/gamescreen.vue
@@ -15,6 +15,12 @@ export default {
     },
     gameWin: function() {
 
+    },
+    calcScore(seconds_left) {
+    let timeSplit = this.$props.duration.split(":");
+    let total_time = parseInt(timeSplit[0]) * 60 + parseInt(timeSplit[1]);
+    let time_taken = total_time - seconds_left;
+    console.log(time_taken + 's');
     }
   }
 }
@@ -27,7 +33,7 @@ export default {
       <Game v-bind:icon="icon" v-on:game-win="gameWin"/>
     </div>
     <div class="opacity-4 display-panel" style="background-color:white;">
-      <Countdown v-bind:duration="duration" v-on:game-over="gameOver" />
+      <Countdown v-bind:duration="duration" v-on:game-over="gameOver" v-on:timer="calcScore"/>
     </div>
   </div>
 </template>
